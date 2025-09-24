43eee1dd418bca54b6bd4a97e2c655d31713f115
Ashman-Sharma <ashmanvvr1@gmail.com>
2025-09-24 14:49:48 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,48 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class reverse {
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println("Original list:");
+        print(head);
+
+        head = reverse(head);
+        System.out.println("Reversed list:");
+        print(head);
+    }
+}
